1e9007eafcbb318e7ac6c720918126546b3f10f6
Ilya Danilov <esozap32@mail.ru>
2022-10-27 23:55:32 +0400

    Ploshad' Treygolnika

---
+++ src/main/java/PloshadTreygolnika.java
@@ -0,0 +1,9 @@
+public class PloshadTreygolnika {
+    public static void main(String[] args){
+        int b = 12;
+        int h = 22;
+        double ploshad =(0.5 * b * h);
+        System.out.println ("Площадь треугольника равна: " + ploshad);
+        }
+    }
+
