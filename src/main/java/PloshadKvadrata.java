5ec3b062e5711f11c3933188c4aa275d47778458
Ilya Danilov <esozap32@mail.ru>
2022-10-27 23:19:03 +0400

    Ploshad' Kvadrata

---
+++ src/main/java/PloshadKvadrata.java
@@ -0,0 +1,8 @@
+public class PloshadKvadrata {
+    public static void main(String[] args){
+        int a = 25;
+        int b = 12;
+        double ploshad = (a * b);
+        System.out.println ("Площадь квадрата равна: " + ploshad);
+    }
+}
