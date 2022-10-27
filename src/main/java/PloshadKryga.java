469f3c34251066036f97661cb4b20245dd4d8b6d
Ilya Danilov <esozap32@mail.ru>
2022-10-27 23:33:09 +0400

    Ploshad' Kryga

---
+++ src/main/java/PloshadKryga.java
@@ -0,0 +1,7 @@
+public class PloshadKryga {
+    public static void main(String[] args){
+        int r = 12;
+        double ploshad = Math.PI * (r * r);
+        System.out.println ("Площадь круга равна: " + ploshad);
+    }
+}
