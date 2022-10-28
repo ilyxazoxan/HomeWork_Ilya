0fec063c11573164035970b8ddd9676846e73aac
Ilya Danilov <esozap32@mail.ru>
2022-10-28 22:45:35 +0400

    TheAreaOfTheTriangle

---
+++ src/main/java/TheAreaOfTheTriangle.java
@@ -0,0 +1,9 @@
+public class TheAreaOfTheTriangle {
+    public static void main(String[] args){
+        int b = 12;
+        int h = 22;
+        double area =(0.5 * b * h);
+        System.out.println ("Площадь треугольника равна: " + area);
+        }
+    }
+
