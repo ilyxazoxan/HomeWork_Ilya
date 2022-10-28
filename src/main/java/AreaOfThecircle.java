9e505430beb263b9772d5428bc1bfde17a682bac
Ilya Danilov <esozap32@mail.ru>
2022-10-28 22:53:15 +0400

    AreaOfThecircle

---
+++ src/main/java/AreaOfThecircle.java
@@ -0,0 +1,7 @@
+public class AreaOfThecircle {
+    public static void main(String[] args){
+        int r = 12;
+        double area = Math.PI * (r * r);
+        System.out.println ("Площадь круга равна: " + area);
+    }
+}
--- src/main/java/PloshadKryga.java
+++
@@ -1,7 +0,0 @@
-public class PloshadKryga {
-    public static void main(String[] args){
-        int r = 12;
-        double ploshad = Math.PI * (r * r);
-        System.out.println ("Площадь круга равна: " + ploshad);
-    }
-}
